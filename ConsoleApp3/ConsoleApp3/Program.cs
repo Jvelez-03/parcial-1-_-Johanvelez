3073cb615affece2ddbbc2699d680e2cd3b144b2
Jvelez-03 <velezospina0312@gmail.com>
2024-09-13 20:18:49 -0500

    punto_2Initial commit

---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,31 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // Solicitar al usuario que ingrese los coeficientes
+        Console.Write("Ingrese el valor de a: ");
+        double a = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Ingrese el valor de b: ");
+        double b = Convert.ToDouble(Console.ReadLine());
+
+        // Verificar las condiciones para determinar el tipo de solución
+        if (a == 0)
+        {
+            if (b == 0)
+            {
+                Console.WriteLine("La ecuación tiene infinitas soluciones.");
+            }
+            else
+            {
+                Console.WriteLine("La ecuación no tiene solución.");
+            }
+        }
+        else
+        {
+            // Calcular la solución de la ecuación
+            double x = -b / a;
+            Console.WriteLine("La solución de la ecuación es: x = " + x);
+        }
+    }
+}
